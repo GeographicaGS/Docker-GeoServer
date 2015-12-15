55e2943df72f527a79cb3bf27e0c155eecb34ea4
Juan Pedro Pérez <jp.alcantara@gstgis.com>
2015-12-15 17:49:58 +0100

    Lots of changes.

--- Dockerfile
+++ Dockerfile
@@ -3,48 +3,27 @@ FROM geographica/apache-tomcat:v8.0.18
 MAINTAINER Juan Pedro Perez "jp.alcantara@geographica.gs"
 
 # Environment
-ENV JAVA_HOME /usr/local/jdk1.7.0_75
-ENV JRE_HOME /usr/local/jdk1.7.0_75/jre
-ENV ANT_HOME /usr/local/apache-ant-1.9.4
-ENV CATALINA_HOME /usr/local/apache-tomcat-8.0.18
-ENV PATH $JAVA_HOME/bin:$JRE_HOME/bin:$ANT_HOME/bin:$PATH
-ENV LD_LIBRARY_PATH /usr/local/lib:$CATALINA_HOME/lib:$LD_LIBRARY_PATH
 ENV GEOSERVER_DATA_DIR /var/geoserver-data
-# To use JMX or not
-ENV JMX false
-# JMX port
-ENV JMX_PORT 3333
-ENV JMX_HOSTNAME localhost
-ENV JMX_CONF_FOLDER $CATALINA_HOME/conf
-# JMX access file
-ENV JMX_ACCESS_FILE $JMX_CONF_FOLDER/jmxremote.access
-# JMX password file
-ENV JMX_PASSWORD_FILE $JMX_CONF_FOLDER/jmxremote.password
+
 # Max heap size
 ENV XMX 1024m
+
 # Initial heap size
 ENV XMS 1024m
+
 # PermSize size
 ENV MAXPERMSIZE 512m
-
 
 # Copy assets
 WORKDIR ${JRE_HOME}
 ADD packages/jai-1_1_3-lib-linux-amd64-jre.bin ./
 ADD packages/jai_imageio-1_1-lib-linux-amd64-jre.bin ./
 ADD packages/UnlimitedJCEPolicyJDK7.zip ./lib/security/
-ADD packages/geoserver-2.6.2-war.zip ${CATALINA_HOME}/webapps/
-ADD packages/compile.sh /usr/local/
+ADD packages/geoserver-2.6.2-war.zip $CATALINA_HOME/webapps/
+ADD packages/compile.sh /usr/local/bin/
+ADD packages/run.sh /usr/local/bin/
+RUN chmod 777 /usr/local/bin/run.sh
+RUN chmod 777 /usr/local/bin/compile.sh
+RUN /usr/local/bin/compile.sh
 
-# Do everything
-WORKDIR /usr/local/
-RUN chmod 777 compile.sh
-RUN ./compile.sh
-
-VOLUME /var/geoserver-data
-VOLUME /usr/local/apache-tomcat-8.0.18/logs
-
-EXPOSE 8080
-EXPOSE 3333
-EXPOSE 62911
-CMD su -s /bin/bash tomcat -c "$CATALINA_HOME/bin/catalina.sh run"
+VOLUME $GEOSERVER_DATA_DIR
--- packages/compile.sh
+++ packages/compile.sh
@@ -1,31 +1,25 @@
+#!/bin/bash
+
 # Update and download basic packages
-apt-get update && apt-get install unzip
+apt-get update
+apt-get install unzip
 
 # Install JCE Policy
-unzip ${JRE_HOME}/lib/security/UnlimitedJCEPolicyJDK7.zip -d ${JRE_HOME}/lib/security/
-cp ${JRE_HOME}/lib/security/UnlimitedJCEPolicy/* ${JRE_HOME}/lib/security
-rm -Rf ${JRE_HOME}/lib/security/UnlimitedJCEPolicy
-rm -f ${JRE_HOME}/lib/security/UnlimitedJCEPolicyJDK7.zip
+unzip $JRE_HOME/lib/security/UnlimitedJCEPolicyJDK7.zip -d $JRE_HOME/lib/security/
+cp $JRE_HOME/lib/security/UnlimitedJCEPolicy/* $JRE_HOME/lib/security
+rm -Rf $JRE_HOME/lib/security/UnlimitedJCEPolicy
+rm -f $JRE_HOME/lib/security/UnlimitedJCEPolicyJDK7.zip
 
 # Install JAI and ImageIO
-cd ${JRE_HOME}
-chmod 755 ${JRE_HOME}/jai-1_1_3-lib-linux-amd64-jre.bin
-chmod 755 ${JRE_HOME}/jai_imageio-1_1-lib-linux-amd64-jre.bin
+cd $JRE_HOME
+chmod 755 $JRE_HOME/jai-1_1_3-lib-linux-amd64-jre.bin
+chmod 755 $JRE_HOME/jai_imageio-1_1-lib-linux-amd64-jre.bin
 yes | ./jai-1_1_3-lib-linux-amd64-jre.bin
 yes | ./jai_imageio-1_1-lib-linux-amd64-jre.bin
 rm -f ./jai*.bin
 
-# Install GeoServer WAR
-unzip ${CATALINA_HOME}/webapps/geoserver-2.6.2-war.zip -d ${CATALINA_HOME}/webapps/
-chown -R tomcat:tomcat ${CATALINA_HOME}
-chmod 665 ${CATALINA_HOME}/webapps/geoserver.war
-mkdir ${GEOSERVER_DATA_DIR}
-
 # Configure user and group
-chown -R tomcat:tomcat ${GEOSERVER_DATA_DIR}
-useradd --shell /bin/sh --home ${GEOSERVER_DATA_DIR} -g tomcat geoserver-admin
-echo "geoserver-admin:geoserver-admin" | chpasswd
-rm -f ${CATALINA_HOME}/webapps/geoserver-2.6.2-war.zip
+mkdir -p $GEOSERVER_DATA_DIR
+chown -R tomcat:tomcat $GEOSERVER_DATA_DIR
+rm -f /usr/local/bin/compile.sh
 
-# Clean up
-rm -Rf /usr/local/compile.sh
